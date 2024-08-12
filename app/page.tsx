cc7394688e6a41bf3c37b166feded1baa87be3f8
Дияр Амангельды <d.amangeldy@arta.pro>
2024-08-12 19:42:47 +0500

    setup project, do header

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Container, Title } from "@/components/shared";
+
+export default function Home() {
+  return (
+    <>
+      <Container className="mt-10">
+        <Title text="Все пиццы" size="lg" className="font-extrabold"></Title>
+      </Container>
+    </>
+  );
+}
