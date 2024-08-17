1a4ae8b1f17e4a8c305e13e589608d2308969d32
Дияр Амангельды <d.amangeldy@arta.pro>
2024-08-17 19:15:30 +0500

    finish aside and display ui products, prepare to backend

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import { Container, SortPopup, Title } from "@/components/shared";
-import { Categories } from "@/components/shared/categories";
 import { Filters } from "@/components/shared/filters";
+import { ProductsGroupList } from "@/components/shared/products-group-list";
 import { TopBar } from "@/components/shared/top-bar";
 
 export default function Home() {
@@ -12,8 +12,8 @@ export default function Home() {
 
       <TopBar />
 
-      <Container className="mt-10 pb-14">
-        <div className="flex gap-[60px]">
+      <Container className="pb-14 mt-3">
+        <div className="flex gap-[80px]">
 
           <div className="w-[250px]">
             <Filters />
@@ -21,7 +21,101 @@ export default function Home() {
 
           <div className="flex-1">
             <div className="flex flex-col gap-16">
-              Список товаров
+              <ProductsGroupList
+                title="Пиццы"
+                items={[
+                  {
+                    id: 1,
+                    name: "Чизбургер пицца",
+                    imageUrl: "https://media.dodostatic.net/image/r:584x584/11EEF45F297D4479903041766B142AB1.avif",
+                    price: 550, 
+                    items: [{ price: 550 }],
+                  },
+                  {
+                    id: 2,
+                    name: "Чизбургер пицца",
+                    imageUrl: "https://media.dodostatic.net/image/r:584x584/11EEF45F297D4479903041766B142AB1.avif",
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+                  {
+                    id: 3,
+                    name: "Чизбургер пицца",
+                    imageUrl: "https://media.dodostatic.net/image/r:584x584/11EEF45F297D4479903041766B142AB1.avif",
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+                  {
+                    id: 4,
+                    name: "Чизбургер пицца",
+                    imageUrl: "https://media.dodostatic.net/image/r:584x584/11EEF45F297D4479903041766B142AB1.avif",
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+
+                  {
+                    id: 5,
+                    name: "Чизбургер пицца",
+                    imageUrl: "https://media.dodostatic.net/image/r:584x584/11EEF45F297D4479903041766B142AB1.avif",
+                    price: 550,
+                    items: [{ price: 550 }],
+                  }, {
+                    id: 6,
+                    name: "Чизбургер пицца",
+                    imageUrl: "https://media.dodostatic.net/image/r:584x584/11EEF45F297D4479903041766B142AB1.avif",
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+                ]}
+                categoryId={1} />
+                 
+              <ProductsGroupList
+                title="Комбо"
+                items={[
+                  {
+                    id: 1,
+                    name: "Чизбургер пицца",
+                    imageUrl: "https://media.dodostatic.net/image/r:584x584/11EEF45F297D4479903041766B142AB1.avif",
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+                  {
+                    id: 2,
+                    name: "Чизбургер пицца",
+                    imageUrl: "https://media.dodostatic.net/image/r:584x584/11EEF45F297D4479903041766B142AB1.avif",
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+                  {
+                    id: 3,
+                    name: "Чизбургер пицца",
+                    imageUrl: "https://media.dodostatic.net/image/r:584x584/11EEF45F297D4479903041766B142AB1.avif",
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+                  {
+                    id: 4,
+                    name: "Чизбургер пицца",
+                    imageUrl: "https://media.dodostatic.net/image/r:584x584/11EEF45F297D4479903041766B142AB1.avif",
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+
+                  {
+                    id: 5,
+                    name: "Чизбургер пицца",
+                    imageUrl: "https://media.dodostatic.net/image/r:584x584/11EEF45F297D4479903041766B142AB1.avif",
+                    price: 550,
+                    items: [{ price: 550 }],
+                  }, {
+                    id: 6,
+                    name: "Чизбургер пицца",
+                    imageUrl: "https://media.dodostatic.net/image/r:584x584/11EEF45F297D4479903041766B142AB1.avif",
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+                ]}
+                categoryId={2} />
             </div>
           </div>
         </div>
---
+++ components/shared/product-card.tsx
@@ -0,0 +1,38 @@
+import {  Plus } from 'lucide-react';
+import React from 'react';
+import { Title } from './title';
+import { Button } from '../ui';
+import Image from 'next/image';
+import Link from 'next/link';
+
+interface Props {
+    id: number;
+    name: string;
+    price: number;
+    imageUrl: string;
+    className?: string;
+}
+
+export const ProductCard: React.FC<Props> = ({ id, name, price, imageUrl, className }) => {
+    return (
+        <div className={className}>
+            <div className="flex justify-center p-6 bg-secondary rounded-lg h-[260px]">
+                    <img className="w-[215px] h-[215px]" src={imageUrl} alt={name} />
+                </div>
+                <Link href={`/product/${id}`}>HAHhaa</Link>
+                <Title text={name} size="sm" className="mb-1 mt-3 font-bold" />
+                <p className="text-sm text-gray-400">
+                    efhgwuishkjbdfduikhnbhgjf ejdcbhuujhmvdsuyjcmxvh hsjgyx
+                </p>
+                <div className="flex justify-between items-center mt-4">
+                    <span className="text-[20px]">
+                        от <b>{price} ₽</b>
+                    </span>
+                    <Button variant="secondary" className="text-base font-bold">
+                        <Plus size={20} className="mr-1" />
+                        Добавить
+                    </Button>
+            </div>
+        </div>
+    );
+};
